cd9b534c54891806b2701507df52a1d43fd41314
Gudine <gudine283@gmail.com>
2022-02-11 17:10:52 -0300

    Adiciona box principal do jogo

--- script.js
+++ script.js
@@ -2,8 +2,53 @@
 const buttonStart = document.querySelector('.start');
 
 // Funções
+const createAnswerSection = () => {
+  const section = document.createElement('section');
+
+  const answerBox = document.createElement('input');
+  answerBox.className = 'game-input';
+  section.appendChild(answerBox);
+
+  const guessBtn = document.createElement('button');
+  guessBtn.innerText = 'Guess';
+  guessBtn.className = 'game-button';
+  section.appendChild(guessBtn);
+
+  const hintBtn = document.createElement('button');
+  hintBtn.innerText = 'Hint';
+  hintBtn.className = 'game-button';
+  section.appendChild(hintBtn);
+
+  return section;
+}
+
+const createGameContainer = () => {
+  buttonStart.remove();
+  const principal = document.querySelector('.principal');
+
+  const gameContainer = document.createElement('section');
+  gameContainer.className = 'game-container';
+
+  const img = document.createElement('img');
+  img.src = 'https://upload.wikimedia.org/wikipedia/commons/9/90/Boa_constrictor,_Vaňkovka,_Brno_(2).jpg';
+  img.className = 'game-main-img';
+  gameContainer.appendChild(img);
+
+  const answerSection = createAnswerSection();
+  gameContainer.appendChild(answerSection);
+
+  const hints = document.createElement('ol');
+  hints.classList = 'game-list';
+  const hint = document.createElement('li');
+  hint.innerText = 'teste';
+  hints.appendChild(hint);
+  gameContainer.appendChild(hints);
+
+  principal.appendChild(gameContainer);
+}
+
 const startGame = () => {
-  
+  createGameContainer();
 } 
 
 window.onload = () => {
