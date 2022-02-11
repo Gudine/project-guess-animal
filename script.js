76d2ef532f65ddaeb42a1e2dc7afc14962973cd5
Pedro Fideles <pedro.fideles162@gmail.com>
2022-02-11 16:24:33 -0300

    Consertando commits anteriores

---
+++ script.js
@@ -0,0 +1,11 @@
+// Seletores e variáveis
+const buttonStart = document.querySelector('.start');
+
+// Funções
+const startGame = () => {
+  
+} 
+
+window.onload = () => {
+  buttonStart.addEventListener('click', startGame);
+}
