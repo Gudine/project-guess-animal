f424dcb43f3f44a0699435c9afa1f78ce158ae43
Ju_M <jm.mnotti@gmail.com>
2022-02-12 15:26:58 -0300

    Refatoração do código e uso do API

--- script.js
+++ script.js
@@ -2,40 +2,52 @@
 const buttonStart = document.querySelector('.start');
 
 // Funções
+const createGameElement = (element, className, text = null, callbackListener) => {
+  const elem = document.createElement(element);
+  elem.innerText = text;
+  elem.className = className;
+  
+  if (callbackListener) {
+    elem.addEventListener('click', callbackListener);
+  }
+  return elem;
+}
+
+const tryToGuess = () => {
+
+}
+
+const getHint = () => {
+  
+}
+
 const createAnswerSection = () => {
   const section = document.createElement('section');
 
-  const answerBox = document.createElement('input');
-  answerBox.className = 'game-input';
-  section.appendChild(answerBox);
-
-  const guessBtn = document.createElement('button');
-  guessBtn.innerText = 'Guess';
-  guessBtn.className = 'game-button';
-  section.appendChild(guessBtn);
-
-  const hintBtn = document.createElement('button');
-  hintBtn.innerText = 'Hint';
-  hintBtn.className = 'game-button';
-  section.appendChild(hintBtn);
+  section.appendChild(createGameElement('input', 'game-input'));
+  section.appendChild(createGameElement('button', 'game-button', 'Guess', tryToGuess));
+  section.appendChild(createGameElement('button', 'game-button', 'Hint', getHint));
 
   return section;
 }
 
-const createGameContainer = () => {
+const createGameImage = (image) => {
+  const img = document.createElement('img');
+  img.src = image;
+  img.className = 'game-main-img';
+  return img;
+}
+
+const createGameContainer = (data) => {
   buttonStart.remove();
   const principal = document.querySelector('.principal');
 
   const gameContainer = document.createElement('section');
   gameContainer.className = 'game-container';
 
-  const img = document.createElement('img');
-  img.src = 'https://upload.wikimedia.org/wikipedia/commons/9/90/Boa_constrictor,_Vaňkovka,_Brno_(2).jpg';
-  img.className = 'game-main-img';
-  gameContainer.appendChild(img);
+  gameContainer.appendChild(createGameImage(data.image_link));
 
-  const answerSection = createAnswerSection();
-  gameContainer.appendChild(answerSection);
+  gameContainer.appendChild(createAnswerSection());
 
   const hints = document.createElement('ol');
   hints.classList = 'game-list';
@@ -47,8 +59,9 @@ const createGameContainer = () => {
   principal.appendChild(gameContainer);
 }
 
-const startGame = () => {
-  createGameContainer();
+const startGame = async () => {
+  const data = await fetchAnimal();
+  createGameContainer(data);
 } 
 
 window.onload = () => {
